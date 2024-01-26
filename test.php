ae8e0a9da2e230321482d4899c167097d15a95e8
cenneimone <nattakit.bbm61@gmail.com>
2024-01-26 22:56:13 +0700

    add

--- test.php
+++ test.php
@@ -1,8 +1,1 @@
-<?php
-    include("template.html")
-?>
-
-<div>
-    <input type="password" placeholder="password" class="form-control" >
-    <img src="img/eye-close.png" alt="" width="25">
-</div>
+<h1>Kuy</h1>
